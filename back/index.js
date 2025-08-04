1433729c5c12e99300f951884168c69d47034ba1
jeevant010 <jeevantmudgil10@gmail.com>
2025-08-04 12:38:06 +0530

    user model starter content will discuss the real factors with all other schema then create the complete

--- back/index.js
+++ back/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const port = 8000;
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 app.get( "/" , (req, res) => res.send( " Can you see me?" ) );
