c3b491b9b3f3fa422c98a9817fc8cafbb3e92585
jeevant010 <jeevantmudgil10@gmail.com>
2025-08-06 09:57:27 +0530

    setup is ready

--- back/index.js
+++ back/index.js
@@ -5,6 +5,8 @@ const passport = require("passport");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+
+
 const app = express();
 app.get( "/" , (req, res) => res.send( " Can you see me?" ) );
 app.listen( port , () => console.log("App is running on port http://localhost:" + port));
