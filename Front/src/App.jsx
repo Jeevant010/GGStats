1ffb7999a50d9f83848251b51d7c5033d3f4086d
jeevant010 <jeevantmudgil10@gmail.com>
2025-08-06 10:57:50 +0530

    Frontend implemented to home page

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-red-200'> hello </div>
+    <div> hello</div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ Front/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        
+    <>
+        <div className="bg-black-700">
+           <p>is dashboard visible?</p> 
+        </div>
+        <div>
+            hello
+        </div>
+    </>
+
+    )};
+
+export default Home;
