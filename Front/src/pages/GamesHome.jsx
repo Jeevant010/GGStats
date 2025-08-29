a10633e30026efad9d01e530c525f6b82b114849
Deepesh70 <deepeshdangi700@gmail.com>
2025-08-29 23:50:30 +0530

    GamePage background fixes

--- Front/src/pages/GamesHome.jsx
+++ Front/src/pages/GamesHome.jsx
@@ -4,15 +4,17 @@ import Header from '../components/Differ/single/Header';
 const GamesHome = () => {
   return (
     <>
+      <Header />
+    <div className='bg-black opacity-100'>
       <div
-        className="relative min-h-screen h-full w-full bg-cover bg-center bg-opacity-10"
+        className="relative min-h-screen h-full w-full bg-cover bg-center opacity-50"
         style={{ backgroundImage: "url('/3dcharacter1.png')" }}
       >
         
         <div className="relative z-10">
-          <Header />
-          <div className='bg-red-600'>GamesHome</div>
+          <h1 className="text-white text-4xl font-bold">Games Home</h1>
         </div>
+      </div>
       </div>
     </>
   );
