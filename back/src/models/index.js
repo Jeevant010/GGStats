28489f30d8f6537214e7f23f6700d654f5c4d28d
Deepesh70 <deepeshdangi700@gmail.com>
2026-06-05 19:02:05 +0530

    upcoming tournament model creation

--- back/src/models/index.js
+++ back/src/models/index.js
@@ -1,9 +1,11 @@
 const User = require('./user.model');
 const AdminModel = require('./admin.model');
 const Game = require('./game.model');
+const Tournament = require('./tournament.model');
 
 module.exports = {
   User,
   AdminModel,
   Game,
+  Tournament,
 };
---
+++ back/src/models/tournament.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const tournamnentSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    sport: {
+        type: String,
+        required: true,
+    },
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    endDate: {
+        type: Date,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    coverImage: {
+        type: String,
+    },
+    videoUrl: {
+        type: String,
+    },
+    tage: [{
+        type: String,
+    }]
+}, {
+    timestamps: true
+});
+
+const Tournament = mongoose.model('Tournament', tournamnentSchema);
+module.exports = Tournament;
